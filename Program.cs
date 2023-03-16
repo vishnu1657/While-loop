78189f512aba4acfb0a290c77863ab9ec477fb2d
HP <HP@DESKTOP-4Q8V1JF>
2023-03-17 01:07:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 10 )
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
